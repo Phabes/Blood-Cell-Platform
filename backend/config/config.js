44ab3e136c0c86156e23581d1e5f73573261a46b
Phabes <quinlanvos1234@gmail.com>
2023-05-08 21:03:17 +0200

    config added

---
+++ backend/config/config.js
@@ -0,0 +1,12 @@
+const CONNECTION_URL =
+  "mongodb+srv://admin:admin@ioioioio.xtsmdgp.mongodb.net/IOIOIOIO";
+const SECRET = "AUTHKEY";
+const MAX_AGE = 3 * 24 * 60 * 60;
+const GITHUB_TOKEN = "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";
+
+module.exports = {
+  CONNECTION_URL,
+  SECRET,
+  MAX_AGE,
+  GITHUB_TOKEN,
+};
