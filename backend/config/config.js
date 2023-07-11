39c1db4088a486fc2f9763990e427d624c7cd5f8
Phabes <quinlanvos1234@gmail.com>
2023-07-11 18:43:38 +0200

    token updated

--- backend/config/config.js
+++ backend/config/config.js
@@ -2,7 +2,7 @@ const CONNECTION_URL =
   "mongodb+srv://admin:admin@ioioioio.xtsmdgp.mongodb.net/IOIOIOIO";
 const SECRET = "AUTHKEY";
 const MAX_AGE = 3 * 24 * 60 * 60;
-const GITHUB_TOKEN = "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";
+const GITHUB_TOKEN = "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";
 
 module.exports = {
   CONNECTION_URL,
