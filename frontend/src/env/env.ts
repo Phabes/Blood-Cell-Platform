0bf971d3662f4c0546141fba4ef7fa489a37768b
Phabes <quinlanvos1234@gmail.com>
2023-07-11 18:19:28 +0200

    bad url fixed

--- frontend/src/env/env.ts
+++ frontend/src/env/env.ts
@@ -1,5 +1,5 @@
 // DEPLOYED
-export const SERVER_NAME = "https://lucky-salamander-68ba59.netlify.app";
+export const SERVER_NAME = "https://blood-cell-platform.onrender.com";
 // export const SERVER_NAME = 'https://ioioioiobackend.azurewebsites.net'; // gumis deploy
 // DOCKER and LOCAL
 // export const SERVER_NAME = "http://localhost:4000";
